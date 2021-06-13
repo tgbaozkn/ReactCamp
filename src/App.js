91caee298c0b9d9afc5ed7f3eaac20ed0de1ad5d
Tugba <39098806+tgbaozkn@users.noreply.github.com>
2021-06-13 18:43:34 +0300

    5 June

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Dashboard from './layouts/Dashboard';
+import 'semantic-ui-css/semantic.min.css' //minify edilmis hali
+//kodlar asla html değildir.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" >
+          <Dashboard></Dashboard>
+
+        </div>
+            );
 }
 
-export default App;
+            export default App;
